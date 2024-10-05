7b7dbbb88373083ed384797c33ec86b6cdb066b2
Awangku Muhammad Syazwan bin Awangku Sazali <aasazali@seekasia.com>
2024-10-05 09:46:05 +0800

    fix: Revert Router config in App

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import HomePage from "./pages/Home.tsx";
 import SessionsPage from "./pages/Sessions.tsx";
 import SessionPage from "./pages/Session.tsx";
 import Root from "./pages/Root.tsx";
-import { SessionContextProvider } from "./store/sessions-context.tsx";
 
 const Router = createBrowserRouter([
   {
@@ -23,9 +22,7 @@ const Router = createBrowserRouter([
 
 function App() {
   return (
-    <SessionContextProvider>
-      <RouterProvider router={Router} />;
-    </SessionContextProvider>
+    <RouterProvider router={Router} />
   );
 }
 
