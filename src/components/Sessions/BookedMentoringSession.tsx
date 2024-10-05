9547cd3c7cb98415037a2271ba65c0bbd022d670
Awangku Muhammad Syazwan bin Awangku Sazali <aasazali@seekasia.com>
2024-10-05 09:54:50 +0800

    feat: Create button component -wip

--- src/components/Sessions/BookedMentoringSession.tsx
+++ src/components/Sessions/BookedMentoringSession.tsx
@@ -1,24 +1,37 @@
-
-
 interface BookedMentoringSessionProps {
-    session: {
-        id: string;
-        title: string;
-        description: string;
-        date: Date;
-        time: string;
-    }
-    onCancel: (sessionId: string) => void;
+  session: {
+    id: string;
+    title: string;
+    description: string;
+    date: Date;
+    durationInMinutes: number;
+  };
+  onCancel: () => void;
 }
 
-export default BookedMentoringSession = (
-  props: BookedMentoringSessionProps
-) => {
-    const [bookedSessions, setBookedSessions] = useState<bookedSessions>([])
+export default BookedMentoringSession = ({
+  session,
+  onCancel,
+}: BookedMentoringSessionProps) => {
+  const [bookedSessions, setBookedSessions] = useState<bookedSessions>([]);
 
-    return (
+  return (
+    <article>
     <div>
-        <h4>Booked Mentoring Sessions</h4>
+      <h4>{session.title}</h4>
+      <p>{session.description}</p>
+      <time dateTime={session.date.toString()}>
+        {session.date.toLocaleDateString('en-MY', {
+            year: '2-digit',
+            month: 'short',
+            day: '2-digit',
+        })}
+      </time>
+      <p>{session.durationInMinutes} minutes</p>
     </div>
-    );
+    <Button textOnly onClick={() => onCancel()}>
+        Cancel session
+    </Button>
+    </article>
+  );
 };
