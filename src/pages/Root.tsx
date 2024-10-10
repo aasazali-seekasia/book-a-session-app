413fe2eeb1c6f609004b14f9522dc917cc0d3b5f
Awangku Muhammad Syazwan bin Awangku Sazali <aasazali@seekasia.com>
2024-10-10 14:32:25 +0800

    feat: Wrap root elements with context provider

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import NavHeader from "../components/Navigation/NavHeader";
+import { SessionContextProvider } from "../store/sessions-context";
 
 export default function Root() {
   return (
-    <>
+    <SessionContextProvider>
       <NavHeader />
       <Outlet />
-    </>
+    </SessionContextProvider>
   );
 }
